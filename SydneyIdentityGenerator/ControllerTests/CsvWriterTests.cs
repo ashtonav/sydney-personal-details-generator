b60894ca75f95d57c3a0ca8135d73bea60630f43
Ashton Abdiukov <ashton@abdiukov.com>
2025-09-17 20:26:56 -0700

    feat: setup cicd that collects test code coverage (#2)

--- SydneyIdentityGenerator/ControllerTests/CsvWriterTests.cs
+++ SydneyIdentityGenerator/ControllerTests/CsvWriterTests.cs
@@ -43,5 +43,5 @@ public class CsvWriterTests
             File.Delete(fileName);
         }
     }
-    
+
 }
--- SydneyIdentityGenerator/ControllerTests/Models/PersonTests.cs
+++ SydneyIdentityGenerator/ControllerTests/Models/PersonTests.cs
@@ -125,5 +125,5 @@ public class PersonTests
 
 public class PersonToTest : Person
 {
-    
+
 }
